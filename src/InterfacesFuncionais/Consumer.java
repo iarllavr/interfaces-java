40757e5a46af7da2849dd094d739c80f0dddd87a
Iarlla Vieira <iarllavieirareis@gmail.com>
2024-10-15 22:37:37 -0300

    iniciando estudo de interfaces
    Signed-off-by: Iarlla Vieira <iarllavieirareis@gmail.com>

---
+++ src/InterfacesFuncionais/Consumer.java
@@ -0,0 +1,29 @@
+package InterfacesFuncionais;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Consumer {
+
+    public static void main(String[] args) {
+
+        //criar uma lista de números inteiros
+
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5);
+
+        //usar o Consumer como expressão lambda para imprimir números pares
+        java.util.function.Consumer<Integer> imprimirNumeroPar = numero -> {
+            if (numero % 2 == 0) {
+                System.out.println(numero);
+            }
+        };
+
+        //usar o Consumer para imprimir números pares
+//        numeros.stream().forEach(imprimirNumeroPar);
+        numeros.forEach(n -> {
+            if (n % 2 == 0) {
+                System.out.println(n);
+            }
+        });
+    }
+}
